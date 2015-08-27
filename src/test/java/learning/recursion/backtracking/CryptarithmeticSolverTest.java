96277f55a06becda17b1cc2be77aa29abe9fbdcb
chethan prakash <chethanp@yahoo-inc.com>
2015-08-26 19:57:02 -0700

    Added the CryptarithmeticSolver ( backtracking )

--- src/test/java/learning/recursion/backtracking/CryptarithmeticSolverTest.java
+++ src/test/java/learning/recursion/backtracking/CryptarithmeticSolverTest.java
@@ -15,5 +15,7 @@ public class CryptarithmeticSolverTest {
     public void testSolve() throws Exception {
         assertThat(new TreeMap<>(new CryptarithmeticSolver().solve("SEND", "MORE", "MONEY")).toString()).
                 isEqualTo("{D=7, E=5, M=1, N=6, O=0, R=8, S=9, Y=2}");
+        assertThat(new TreeMap<>(new CryptarithmeticSolver().solve("swathi", "chethan", "talents")).toString()).
+                isEqualTo("{a=6, c=2, e=0, h=7, i=8, l=5, n=1, s=9, t=3, w=4}");
     }
 }
