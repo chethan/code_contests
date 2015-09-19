0372dfec1b588c557fedc1e529b436476a617c22
chethan prakash <chethanp@yahoo-inc.com>
2015-09-19 02:15:09 -0700

    Champagne fountain puzzle

--- src/test/java/learning/interview/CChampagneFountainTest.java
+++ src/test/java/learning/interview/CChampagneFountainTest.java
@@ -7,11 +7,11 @@ import static org.assertj.core.api.StrictAssertions.assertThat;
 /**
  * Created by Chethan on 9/19/15.
  */
-public class ChampagneFountainTest {
+public class CChampagneFountainTest {
 
     @Test
     public void testGetChampagneIn() throws Exception {
-        ChampagneFountain ChampagneFountain = new ChampagneFountain();
+        CChampagneFountain ChampagneFountain = new CChampagneFountain();
         assertThat(ChampagneFountain.getChampagneIn(2, 2)).isEqualTo(0.5);
         assertThat(ChampagneFountain.getChampagneIn(5, 5)).isEqualTo(1.0);
         assertThat(ChampagneFountain.getChampagneIn(7, 8)).isEqualTo(0.5);
