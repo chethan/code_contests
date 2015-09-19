4ca1eda79c1245245d3aad4337c88bca7c5091aa
chethan prakash <chethanp@yahoo-inc.com>
2015-09-19 02:13:46 -0700

    ChampageFoundtain puzzle

---
+++ src/main/java/learning/interview/champagneFountain.java
@@ -0,0 +1,25 @@
+package learning.interview;
+
+/**
+ * Created by Chethan on 9/19/15.
+ */
+//http://programmers.stackexchange.com/questions/184994/champaign-fountain-puzzle
+public class ChampagneFountain {
+    public double getChampagneIn(int quantity, int number) {
+        int glassNumber = 1;
+        double[] champagneIn = new double[number + 1];
+        champagneIn[1] = quantity;
+        for (int level = 1; glassNumber <= number; level++) {
+            for (int i = 0; glassNumber <= number && i < level; i++, glassNumber++) {
+                double overflow = champagneIn[glassNumber] - 1;
+                if (overflow > 0) {
+                    if (glassNumber + level <= number) champagneIn[glassNumber + level] += overflow / 2;
+                    if (glassNumber + level + 1 <= number) champagneIn[glassNumber + level+1] += overflow / 2;
+                    champagneIn[glassNumber] = 1;
+                }
+            }
+        }
+        return champagneIn[number];
+    }
+
+}
---
+++ src/test/java/learning/interview/champagneFountainTest.java
@@ -0,0 +1,22 @@
+package learning.interview;
+
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.StrictAssertions.assertThat;
+
+/**
+ * Created by Chethan on 9/19/15.
+ */
+public class ChampagneFountainTest {
+
+    @Test
+    public void testGetChampagneIn() throws Exception {
+        ChampagneFountain ChampagneFountain = new ChampagneFountain();
+        assertThat(ChampagneFountain.getChampagneIn(2, 2)).isEqualTo(0.5);
+        assertThat(ChampagneFountain.getChampagneIn(5, 5)).isEqualTo(1.0);
+        assertThat(ChampagneFountain.getChampagneIn(7, 8)).isEqualTo(0.5);
+        assertThat(ChampagneFountain.getChampagneIn(7, 7)).isEqualTo(0.0);
+    }
+
+
+}
