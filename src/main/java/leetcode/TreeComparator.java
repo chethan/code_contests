e8b923fb18669c8a236830a906e1cf2191ad2086
chethan prakash <chethanp@yahoo-inc.com>
2015-10-24 08:18:15 -0700

    Same tree #leetcode #Tree

--- src/main/java/leetcode/TreeComparator.java
+++ src/main/java/leetcode/TreeComparator.java
@@ -5,10 +5,21 @@ import ds.TreeNode;
 /**
  * Created by Chethan on 10/24/15.
  */
-//https://leetcode.com/problems/symmetric-tree/
-public class SymmetricTree {
+
+public class TreeComparator {
+    //https://leetcode.com/problems/symmetric-tree/
     public boolean isSymmetric(TreeNode root) {
         return root == null || isSymmetric(root.left, root.right);
+    }
+
+    //https://leetcode.com/problems/same-tree/
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) return true;
+        if (p == null || q == null) return false;
+        return (p.val == q.val &&
+                isSameTree(p.left, q.left) &&
+                isSameTree(p.right, q.right));
+
     }
 
     private boolean isSymmetric(TreeNode tree1, TreeNode tree2) {
--- src/test/java/leetcode/TreeComparatorTest.java
+++ src/test/java/leetcode/TreeComparatorTest.java
@@ -8,18 +8,19 @@ import static org.assertj.core.api.Assertions.assertThat;
 /**
  * Created by Chethan on 10/24/15.
  */
-public class SymmetricTreeTest {
+public class TreeComparatorTest {
 
     @Test
-    public void testIsSymmetric() throws Exception {
-        SymmetricTree symmetricTree = new SymmetricTree();
+    public void testTreeComparision() throws Exception {
+        TreeComparator treeComparator = new TreeComparator();
         TreeNode tree = TreeNode.builder().val(1).
                 left(TreeNode.builder().val(2).
                         left(TreeNode.builder().val(4).build()).build()).
                 right(TreeNode.builder().val(2).
                         right(TreeNode.builder().val(4).build()).build()).build();
 
-        assertThat(symmetricTree.isSymmetric(tree)).isTrue();
+        assertThat(treeComparator.isSymmetric(tree)).isTrue();
+        assertThat(treeComparator.isSameTree(tree, tree)).isTrue();
 
     }
 }
