ceaa812bf5d920ce59125959ee81c1e5ec88dd0d
cp <chethan.prakash@navi.com>
2021-05-23 01:47:16 +0530

    Chethan | Solution for more island problems

--- src/main/java/leetcode/graph/Islands.java
+++ src/main/java/leetcode/graph/Islands.java
@@ -1,11 +1,15 @@
 package leetcode.graph;
 
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Set;
 
-//https://leetcode.com/problems/number-of-islands/
 public class Islands {
 
+    private static final int[][] directions = {{0, -1}, {-1, 0}, {0, 1}, {1, 0}};
+
+    //https://leetcode.com/problems/number-of-islands/
     public int numIslands(char[][] grid) {
         if (grid.length == 0) {
             return 0;
@@ -15,7 +19,7 @@ public class Islands {
         for (int row = 0; row < grid.length; row++) {
             for (int col = 0; col < grid[0].length; col++) {
                 if (!visited.contains(row + "_" + col) && grid[row][col] == '1') {
-                    bfs(grid, row, col, visited);
+                    dfs(grid, row, col, visited);
                     numberOfIslands++;
                 }
             }
@@ -23,10 +27,43 @@ public class Islands {
         return numberOfIslands;
     }
 
-    void bfs(char[][] grid, int row, int col, Set<String> visited) {
-        visited.add(row + "_" + col);
-        int[][] directions = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
+    //https://leetcode.com/problems/max-area-of-island/
+    public int maxAreaOfIsland(int[][] grid) {
+        int rowSize = grid.length;
+        if (rowSize == 0) {
+            return 0;
+        }
+        int colSize = grid[0].length;
+        int maxArea = 0;
+        for (int i = 0; i < rowSize; i++) {
+            for (int j = 0; j < colSize; j++) {
+                if (grid[i][j] == 1) {
+                    maxArea = Math.max(maxArea, areaOfIsland(grid, i, j));
+                }
+            }
+        }
+        return maxArea;
+    }
 
+    private int areaOfIsland(int[][] grid, int row, int col) {
+        int area = 1;
+        grid[row][col] = 2;
+
+        for (int[] direction : directions) {
+            int newRow = row + direction[0];
+            int newCol = col + direction[1];
+            if (newRow < 0 || newCol < 0 || newRow >= grid.length || newCol >= grid[0].length) {
+                continue;
+            }
+            if (grid[newRow][newCol] == 1) {
+                area += areaOfIsland(grid, newRow, newCol);
+            }
+        }
+        return area;
+    }
+
+    void dfs(char[][] grid, int row, int col, Set<String> visited) {
+        visited.add(row + "_" + col);
         for (int[] direction : directions) {
             int nextRow = row + direction[0];
             int nextCol = col + direction[1];
@@ -35,7 +72,75 @@ public class Islands {
                 || grid[nextRow][nextCol] == '0' || visited.contains(nextRow + "_" + nextCol)) {
                 continue;
             }
-            bfs(grid, nextRow, nextCol, visited);
+            dfs(grid, nextRow, nextCol, visited);
+        }
+    }
+
+    //https://leetcode.com/problems/shortest-bridge/
+    public int shortestBridge(int[][] grid) {
+        int rowLength = grid.length;
+        if (rowLength == 0) {
+            return 0;
+        }
+        int colLength = grid[0].length;
+        Queue<int[]> island = getIslandNodes(grid, rowLength, colLength);
+        int bridgeLength = 0;
+        while (!island.isEmpty()) {
+            int size = island.size();
+            for (int i = 0; i < size; i++) {
+                int[] current = island.poll();
+                for (int[] direction : directions) {
+                    int newRow = current[0] + direction[0];
+                    int newCol = current[1] + direction[1];
+                    if (newRow < 0 || newCol < 0 || newRow >= grid.length || newCol >= grid[0].length || grid[newRow][newCol] == 2) {
+                        continue;
+                    }
+
+                    if (grid[newRow][newCol] == 1) {
+                        return bridgeLength;
+                    }
+                    island.offer(new int[]{newRow, newCol});
+                    grid[newRow][newCol] = 2;
+                }
+
+
+            }
+            bridgeLength++;
+        }
+        return -1;
+    }
+
+    private Queue<int[]> getIslandNodes(int[][] grid, int rowLength, int colLength) {
+        Queue<int[]> island = new LinkedList<>();
+        for (int i = 0; i < rowLength; i++) {
+            if (!island.isEmpty()) {
+                break;
+            }
+            for (int j = 0; j < colLength; j++) {
+                if (grid[i][j] == 1) {
+                    grid[i][j] = 2;
+                    island.offer(new int[]{i, j});
+                    islandNodes(grid, i, j, island);
+                    break;
+                }
+            }
+        }
+        return island;
+    }
+
+
+    void islandNodes(int[][] grid, int row, int col, Queue<int[]> island) {
+        for (int[] direction : directions) {
+            int newRow = row + direction[0];
+            int newCol = col + direction[1];
+            if (newRow < 0 || newCol < 0 || newRow >= grid.length || newCol >= grid[0].length) {
+                continue;
+            }
+            if (grid[newRow][newCol] == 1) {
+                grid[newRow][newCol] = 2;
+                island.offer(new int[]{newRow, newCol});
+                islandNodes(grid, newRow, newCol, island);
+            }
         }
     }
 }
--- src/test/java/leetcode/graph/IslandsTest.java
+++ src/test/java/leetcode/graph/IslandsTest.java
@@ -2,6 +2,7 @@ package leetcode.graph;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import org.assertj.core.api.StrictAssertions;
 import org.testng.annotations.Test;
 
 public class IslandsTest {
@@ -17,5 +18,21 @@ public class IslandsTest {
         };
         assertThat(islands.numIslands(grid)).isEqualTo(1);
         assertThat(islands.numIslands(new char[][]{{}})).isEqualTo(0);
+    }
+
+    @Test
+    public void testMaxAreaOfIsland() {
+        Islands islands = new Islands();
+        int[][] grid = {{0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0}, {0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0}, {0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0}, {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}};
+        assertThat(islands.maxAreaOfIsland(grid)).isEqualTo(6);
+    }
+
+    @Test
+    public void testShortestBridge() {
+        Islands islands = new Islands();
+        int[][] grid = {{0,1,0},{0,0,0},{0,0,1}};
+        assertThat(islands.shortestBridge(grid)).isEqualTo(2);
     }
 }
