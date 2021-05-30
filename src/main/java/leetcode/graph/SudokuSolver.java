253c2abe52d4325a29d6d0d88b0f7c616329dd0e
cp <chethan.prakash@navi.com>
2021-05-31 01:13:50 +0530

    Chethan | Added Soultion for SudokuSolver

---
+++ src/main/java/leetcode/graph/SudokuSolver.java
@@ -0,0 +1,80 @@
+package leetcode.graph;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+//https://leetcode.com/problems/sudoku-solver/
+public class SudokuSolver {
+
+    private final Map<Integer, Set<Integer>> rowCache = new HashMap<>();
+    private final Map<Integer, Set<Integer>> colCache = new HashMap<>();
+    private final Map<Integer, Set<Integer>> boxCache = new HashMap<>();
+
+    public void solveSudoku(char[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (board[i][j] != '.') {
+                    int digit = Character.getNumericValue(board[i][j]);
+                    placeDigit(digit, i, j, board);
+                }
+            }
+        }
+        solveSudoku(board, 0, 0);
+    }
+
+    private boolean solveSudoku(char[][] board, int row, int col) {
+        if (row >= board.length) {
+            return true;
+        }
+        if (col >= board[0].length) {
+            return solveSudoku(board, row + 1, 0);
+        }
+        if (board[row][col] == '.') {
+            for (int d = 1; d < 10; d++) {
+                if (canPlaceDigit(d, row, col, board)) {
+                    placeDigit(d, row, col, board);
+                    boolean solved = solveSudoku(board, row, col + 1);
+                    if (solved) {
+                        return true;
+                    }
+                    removeDigit(d, row, col, board);
+                }
+            }
+            return false;
+        }
+        return solveSudoku(board, row, col + 1);
+    }
+
+    private boolean canPlaceDigit(int d, int row, int col, char[][] board) {
+        int boxNumber = box(row, col, board.length);
+        return !rowCache.getOrDefault(row, new HashSet<>()).contains(d) &&
+            !colCache.getOrDefault(col, new HashSet<>()).contains(d) &&
+            !boxCache.getOrDefault(boxNumber, new HashSet<>()).contains(d);
+    }
+
+
+    private void placeDigit(int d, int row, int col, char[][] board) {
+        board[row][col] = (char) (d + '0');
+        rowCache.putIfAbsent(row, new HashSet<>());
+        rowCache.get(row).add(d);
+        colCache.putIfAbsent(col, new HashSet<>());
+        colCache.get(col).add(d);
+        int boxNumber = box(row, col, board.length);
+        boxCache.putIfAbsent(boxNumber, new HashSet<>());
+        boxCache.get(boxNumber).add(d);
+    }
+
+    private void removeDigit(int d, int row, int col, char[][] board) {
+        board[row][col] = '.';
+        rowCache.get(row).remove(d);
+        colCache.get(col).remove(d);
+        int boxNumber = box(row, col, board.length);
+        boxCache.get(boxNumber).remove(d);
+    }
+
+    private int box(int row, int col, int n) {
+        return ((row / 3) * 3) + (col / 3);
+    }
+}
